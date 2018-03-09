5bdc918a4ddc7ea603e2b1dd3df7b0e961e9a869
Eric J <wormeyman@gmail.com>
2018-03-09 15:12:38 -0800

    break and const

--- Exercise Files/09_02/JS/script.js
+++ Exercise Files/09_02/JS/script.js
@@ -1,1 +1,9 @@
-
+var extLinks = document.querySelectorAll('a[href^="http"]');
+console.log(extLinks);
+for (var i = 0; i < extLinks.length; i++) {
+  // const element = array[i];
+  // console.log(extLinks[i]);
+  if (!extLinks[i].hasAttribute("target")) {
+    extLinks[i].setAttribute("target", "_blank");
+  }
+}
